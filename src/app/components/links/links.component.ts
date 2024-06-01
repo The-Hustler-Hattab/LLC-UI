d78000bb258a31c5f0fd59d42f001df6c5ec3e80
Mohammed Hattab <mohammedhattab97@gmail.com>
2024-05-31 21:23:02 -0400

    added section 8 links

--- src/app/components/links/links.component.ts
+++ src/app/components/links/links.component.ts
@@ -16,6 +16,15 @@ export class LinksComponent {
 
   ];
 
+  section8Links = [
+    { name: 'HACP - Portal', url: 'https://hacp.org/portal/', image: 'assets/HACP.webp' },
+    { name: 'HACP - Tier Map', url: 'https://alcogis.maps.arcgis.com/apps/webappviewer/index.html?id=531501a0fc54461a8a5ff1ac31bada15', image: 'assets/HACP.webp' },
+    { name: 'HACP - House Tiers', url: 'https://hacp.org/app/uploads/2022/09/Tiered-Payment-Standards-effective-10.1.22.pdf', image: 'assets/HACP.webp' },
+    { name: 'Affordable Housing - Portal', url: 'https://www.affordablehousing.com/v4/pages/login/login.aspx?action=signin&username=&redirect=http%3a%2f%2fwww.affordablehousing.com%2fv4%2fpages%2fListing%2fListing.aspx', image: 'assets/affordableHousing.png' },
+
+
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
